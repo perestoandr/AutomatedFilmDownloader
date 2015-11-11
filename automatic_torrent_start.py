4fabdc68485813307d31fddae3e3ea5fe3362b39
Andrey Perestoronin <per4ik256@gmail.com>
2015-11-11 23:00:08 +0300

    Update subprocess function

--- automatic_torrent_start.py
+++ automatic_torrent_start.py
@@ -6,4 +6,4 @@ from environment import environment
 def run_qbittorrent(torrent_files_list):
     command_string = environment.get('qBittorrent_location') + ' --no-splash '
     command_string += " ".join(torrent_files_list)
-    return subprocess.call(command_string)
+    return subprocess.Popen(command_string)
