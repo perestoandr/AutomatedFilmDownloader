29c3f1c5dad012d6a8537aae39cdab734c103767
Andrey Perestoronin <per4ik256@gmail,com>
2015-11-22 14:58:54 +0300

    Change command string; Edit Popen function call: shlex.split() added

--- automatic_torrent_start.py
+++ automatic_torrent_start.py
@@ -1,9 +1,11 @@
+import shlex
 import subprocess
 
 # from environment_local import environment
 from environment import environment
 
 def run_qbittorrent(torrent_files_list):
-    command_string = environment.get('qBittorrent_location') + ' --no-splash '
+    command_string = environment.get('qBittorrent_location') + ' --daemon --webui-port=8081 '
     command_string += " ".join(torrent_files_list)
-    return subprocess.Popen(command_string)
+    print command_string
+    return subprocess.Popen(shlex.split(command_string))
