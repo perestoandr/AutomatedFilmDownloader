1b6cabdfcf7b367d6c3d8b4032cce2121738a307
Andrey Perestoronin <per4ik256@gmail,com>
2015-11-22 22:39:46 +0300

    Created dropbox database client. Can download and upload json filmbase from Dropbox.com

---
+++ json_db.py
@@ -0,0 +1,36 @@
+import json
+import db_updater as db
+
+db_filename = 'film_base.json'
+
+
+def new_empty_db(dbx_obj, filename):
+    clear_data = {'total': 0, 'records': []}
+    clear_data = json.dumps(clear_data, indent=4)
+    db.upload_data(dbx_obj, clear_data, filename, overwrite=True)
+
+
+def add_to_db(dbx_obj, additional_data, filename):
+    data = read_from_db(dbx_obj, filename)
+    data['records'].extend(additional_data)
+    data['total'] = len(data['records'])
+    data = json.dumps(data, indent=4)
+    db.upload_data(dbx_obj, data, filename, overwrite=True)
+
+
+def read_from_db(dbx_obj, filename):
+    data = db.download(dbx_obj, filename)
+    return json.loads(data)
+
+
+if __name__ == '__main__':
+    dbx = db.set_up_dropbox()
+    name = 'data.json'
+    new_empty_db(dbx, name)
+    print read_from_db(dbx, name)
+    add_to_db(dbx, {'name': name, 'test_data': 'not empty'}, name)
+    print read_from_db(dbx, name)
+    add_to_db(dbx, {'name': name + ', again', 'test_data': 'not empty, again'}, name)
+    print read_from_db(dbx, name)
+    add_to_db(dbx, {'name': name + ', again', 'test_data': ''}, name)
+    print read_from_db(dbx, name)
