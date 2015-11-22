e0f9d9a64881b3eed7fd996be3c6734b905644af
Andrey Perestoronin <per4ik256@gmail,com>
2015-11-22 23:47:21 +0300

    cleared environment.py

--- environment.py
+++ environment.py
@@ -2,13 +2,13 @@ environment = {
     'qBittorrent_location': '/usr/bin/qbittorrent-nox',
     'downloaded_torrents_location': '/media/perestoandr/tbhard/Downloads/TorrentFiles/',
     'downloaded_films_location': '/media/perestoandr/tbhard/Video/',
-    'rutracker_login': 'DumbAssEr',
-    'rutracker_password_base64': 'eGJET0I=',
+    'rutracker_login': '',
+    'rutracker_password_base64': '',
     'downloaded_films_list_file': 'processed_items.txt',
 
-    'dropbox_folder_path': '/home/perestoandr/Dropbox/Apps/RutrackerFilmLoader/',
-    'dropbox_OAuth2_key': 'Qk4063Oqj1sAAAAAAAAniReH_A34nzn7tdNoFxcHbCh-Cix1zJ0C8_Yo6SXUL6qP',
-    'dropbox_app_key': 'qmn544icjafj08x',
-    'dropbox_app_secret': 'jetm85x0pvjr0fi',
+    'dropbox_folder_path': '',
+    'dropbox_OAuth2_key': '',
+    'dropbox_app_key': '',
+    'dropbox_app_secret': '',
     '': ''
 }
